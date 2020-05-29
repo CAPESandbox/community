40aa32a744bf65a807fceb85693accd942ea3505
DoomedRaven <doommedraven@gmail.com>
2020-05-29 15:31:21 +0200

    Update office_suspicious.py

--- modules/signatures/office_suspicious.py
+++ modules/signatures/office_suspicious.py
@@ -59,36 +59,36 @@ class OfficeAnamalousFeature(Signature):
         if "static" in self.results and "office" in self.results["static"]:
             if "Metadata" in self.results["static"]["office"]:
                 if "SummaryInformation" in self.results["static"]["office"]["Metadata"]:
-                    edittime = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["total_edit_time"]
-                    createtime = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["create_time"]
-                    lastsaved = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["last_saved_time"]
-                    if edittime != "None" and int(edittime) > 0 and createtime == "None" and lastsaved == "None":
+                    edittime = self.results["static"]["office"]["Metadata"]["SummaryInformation"].get("total_edit_time")
+                    createtime = self.results["static"]["office"]["Metadata"]["SummaryInformation"].get("create_time")
+                    lastsaved = self.results["static"]["office"]["Metadata"]["SummaryInformation"].get("last_saved_time")
+                    if edittime and int(edittime) > 0 and createtime == "None" and lastsaved == "None":
                         self.data.append({"creation_anomaly" : "The file appears to have an edit time yet has no creation time or last saved time. This can be a sign of an automated document creation kit."})
 
         if "static" in self.results and "office" in self.results["static"]:
             if "Metadata" in self.results["static"]["office"]:
                 if "SummaryInformation" in self.results["static"]["office"]["Metadata"]:
-                    author = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["author"]
-                    lastauthor = self.results["static"]["office"]["Metadata"]["SummaryInformation"]["last_saved_by"]
+                    author = self.results["static"]["office"]["Metadata"]["SummaryInformation"].get("author")
+                    lastauthor = self.results["static"]["office"]["Metadata"]["SummaryInformation"].get("last_saved_by")
                     numerical_author = re.compile("^[0-9]{1,}$")
                     for malicious_author in known_malicious_authors:
                         if author == malicious_author:
                             self.data.append({"malicious_author" : "The file appears to have been created by a known fake author indicative of an automated document creation kit."})
 
-                    if numerical_author.match(author):
+                    if author and numerical_author.match(author):
                         self.data.append({"numerical_author" : "The file author is numerical rather than a word/name indicative of an automated document creation kit."})
 
-                    if re.search("[0-9]{1}", author) and re.search("[A-Z]{1}", author):
+                    if author and re.search("[0-9]{1}", author) and re.search("[A-Z]{1}", author):
                         if len(author) < 6 and re.match("^[a-zA-Z0-9]{1,5}$", author):
                             self.data.append({"short_author_format" : "The file author is a short text string yet contains numerical and upper case characters indicative of an automated document creation kit."})
                         if len(author) > 5 and re.match("^[a-zA-Z0-9].*[A-Z].*$", author):
                            if re.match("^[a-zA-Z0-9]{6,}$", author):
                                self.data.append({"author_format" : "The file author contains a mix of numerical and upper case characters in an unlikely pattern indicative of an automated document creation kit."})
 
-                    if numerical_author.match(lastauthor):
+                    if lastauthor and numerical_author.match(lastauthor):
                         self.data.append({"numerical_last_saved" : "The file was last saved by a numerical author rather than a word/name indicative of an automated document creation kit."})
 
-                    if re.search("[0-9]{1}", lastauthor) and re.search("[A-Z]{1}", lastauthor):
+                    if lastauthor and re.search("[0-9]{1}", lastauthor) and re.search("[A-Z]{1}", lastauthor):
                         if len(lastauthor) < 6 and re.match("^[a-zA-Z0-9]{1,5}$", lastauthor):
                             self.data.append({"short_last_saved_format" : "The file was last saved by an author with short text string containing numerical and upper case characters indicative of an automated document creation kit."})
 
