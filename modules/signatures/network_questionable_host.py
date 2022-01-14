7e7ec8f2a374d5772287f8d033c6cb480da6087e
doomedraven <doommedraven@gmail.com>
2022-01-14 19:39:53 +0100

    Update network_questionable_host.py

--- modules/signatures/network_questionable_host.py
+++ modules/signatures/network_questionable_host.py
@@ -17,6 +17,28 @@ import dns.resolver
 
 from lib.cuckoo.common.abstracts import Signature
 
+RBLs = (
+    "spam.spamrats.com",
+    "web.dnsbl.sorbs.net",
+    "auth.spamrats.com",
+    "http.dnsbl.sorbs.net",
+    "socks.dnsbl.sorbs.net",
+    "misc.dnsbl.sorbs.net",
+    "smtp.dnsbl.sorbs.net",
+    "web.dnsbl.sorbs.net",
+    "zombie.dnsbl.sorbs.net",
+    "block.dnsbl.sorbs.net",
+    "spam.dnsbl.sorbs.net",
+    "noserver.dnsbl.sorbs.net",
+    "escalations.dnsbl.sorbs.net",
+    "noserver.dnsbl.sorbs.net",
+    "zen.spamhaus.org",
+)
+
+resolver = dns.resolver.Resolver()
+resolver.timeout = 0.10
+
+
 class NetworkQuestionableHost(Signature):
     name = "network_questionable_host"
     description = "Communicates with a host in a RBL"
@@ -25,98 +47,28 @@ class NetworkQuestionableHost(Signature):
     categories = ["network", "cnc"]
     authors = ["Zane C. Bowers-Hadley"]
     minimum = "1.3"
+    enabled = False
 
     filter_analysistypes = set(["file"])
 
     def run(self):
-        resolver = dns.resolver.Resolver()
-        resolver.timeout = 0.10
-        RBLs=[
-                'spam.spamrats.com',
-                'web.dnsbl.sorbs.net',
-                'auth.spamrats.com',
-                'http.dnsbl.sorbs.net',
-                'socks.dnsbl.sorbs.net',
-                'misc.dnsbl.sorbs.net',
-                'smtp.dnsbl.sorbs.net',
-                'web.dnsbl.sorbs.net',
-                'zombie.dnsbl.sorbs.net',
-                'block.dnsbl.sorbs.net',
-                'spam.dnsbl.sorbs.net',
-                'noserver.dnsbl.sorbs.net',
-                'escalations.dnsbl.sorbs.net',
-                'noserver.dnsbl.sorbs.net',
-                'zen.spamhaus.org',
-             ]
-        checked={}
-        if "network" in self.results and "hosts" in self.results["network"]:
-            for host in self.results["network"]["hosts"]:
-                ip = host["ip"]
-                checked[ip]=''
-                if not ip.startswith(("10.", "172.16.", "192.168.")):
-                    ipRev =  '.'.join( ip.split('.')[::-1])
-                    for rbl in RBLs:
-                        searchQuery = ipRev+'.'+rbl
-                        try:
-                            resolver.query(searchQuery,'A')
-                            self.data.append({rbl : ip})
-                        except:
-                            print('')
 
-        if "network" in self.results and "tcp" in self.results["network"]:
-            for packet in self.results["network"]["tcp"]:
-                ip = packet["dst"]
-                if not ip.startswith(("10.", "172.16.", "192.168.")) and ip not in checked:
-                    checked[ip]=''
-                    ipRev =  '.'.join( ip.split('.')[::-1])
-                    for rbl in RBLs:
-                        searchQuery = ipRev+'.'+rbl
-                        try:
-                            resolver.query(searchQuery,'A')
-                            self.data.append({rbl : ip})
-                        except:
-                            print('')
-
-        if "network" in self.results and "udp" in self.results["network"]:
-            for packet in self.results["network"]["udp"]:
-                ip = packet["dst"]
-                if not ip.startswith(("10.", "172.16.", "192.168.")) and ip not in checked:
-                    checked[ip]=''
-                    ipRev =  '.'.join( ip.split('.')[::-1])
-                    for rbl in RBLs:
-                        searchQuery = ipRev+'.'+rbl
-                        try:
-                            resolver.query(searchQuery,'A')
-                            self.data.append({rbl : ip})
-                        except:
-                            print('')
-
-        if "network" in self.results and "icmp" in self.results["network"]:
-            for packet in self.results["network"]["icmp"]:
-                ip = packet["dst"]
-                if not ip.startswith(("10.", "172.16.", "192.168.")) and ip not in checked:
-                    checked[ip]=''
-                    ipRev =  '.'.join( ip.split('.')[::-1])
-                    for rbl in RBLs:
-                        searchQuery = ipRev+'.'+rbl
-                        try:
-                            resolver.query(searchQuery,'A')
-                            self.data.append({rbl : ip})
-                        except:
-                            print('')
-                ip = packet["src"]
-                if not ip.startswith(("10.", "172.16.", "192.168.")) and ip not in checked:
-                    checked[ip]=''
-                    ipRev =  '.'.join( ip.split('.')[::-1])
-                    for rbl in RBLs:
-                        searchQuery = ipRev+'.'+rbl
-                        try:
-                            resolver.query(searchQuery,'A')
-                            self.data.append({rbl : ip})
-                        except:
-                            print('')
+        checked = {}
+        for key, value in [("hosts", "ip"), ("tcp", "dst"), ("udp", "dst"), ("icmp", "dst"), ("icmp", "dst")]:
+            for host in self.results.get("network", {}).get(key, []):
+                ip = host[value]
+                checked[ip] = ""
+                if ip.startswith(("10.", "172.16.", "192.168.")):
+                    continue
+                ipRev = ".".join(ip.split(".")[::-1])
+                for rbl in RBLs:
+                    try:
+                        resolver.query(ipRev + "." + rbl, "A")
+                        self.data.append({rbl: ip})
+                    except:
+                        pass
 
         if self.data:
             return True
-        else:
-            return False
+
+        return False
