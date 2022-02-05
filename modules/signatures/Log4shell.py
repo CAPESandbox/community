2a38d6ac1f0178f6ca91ad510c65d5ac6da205ca
Lint Action <lint-action@samuelmeuli.com>
2022-02-05 15:33:41 +0000

    Fix code style issues with Black

--- modules/signatures/Log4shell.py
+++ modules/signatures/Log4shell.py
@@ -20,7 +20,10 @@ except ImportError:
 
 from lib.cuckoo.common.abstracts import Signature
 
-user_agent = re.compile("(?i)((%(25){0,}20|\s)*(%(25){0,}24|\$)(%(25){0,}20|\s)*(%(25){0,}7B|{){0,1}(%(25){0,}20|\s)*(%(25){0,}(6A|4A)|J)(%(25){0,}(6E|4E)|N)(%(25){0,}(64|44)|D)(%(25){0,}(69|49)|I)(%(25){0,}20|\s)*(%(25){0,}3A|:)[\w\%]+(%(25){1,}3A|:)(%(25){1,}2F|\/)|\$((::-[A-Z%]}\$){1,}|(ENV|LOWER|UPPER):).+[:}]{2}\/)[^\n]+")
+user_agent = re.compile(
+    "(?i)((%(25){0,}20|\s)*(%(25){0,}24|\$)(%(25){0,}20|\s)*(%(25){0,}7B|{){0,1}(%(25){0,}20|\s)*(%(25){0,}(6A|4A)|J)(%(25){0,}(6E|4E)|N)(%(25){0,}(64|44)|D)(%(25){0,}(69|49)|I)(%(25){0,}20|\s)*(%(25){0,}3A|:)[\w\%]+(%(25){1,}3A|:)(%(25){1,}2F|\/)|\$((::-[A-Z%]}\$){1,}|(ENV|LOWER|UPPER):).+[:}]{2}\/)[^\n]+"
+)
+
 
 class Log4j(Signature):
     name = "Log4Shell"
@@ -33,7 +36,7 @@ class Log4j(Signature):
         httpitems = self.results.get("network", {}).get("http", [])
         for http in httpitems:
             if user_agent.search(http.get("data", "")):
-                self.data.append({"url" : http["uri"], "user-agent" : http.get("user-agent",""), "data" : http['data']})
+                self.data.append({"url": http["uri"], "user-agent": http.get("user-agent", ""), "data": http["data"]})
                 return True
 
         return False
