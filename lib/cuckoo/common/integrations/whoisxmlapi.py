1a5a86fa0c5f5e8435d6cf19870629e5d2764783
doomedraven <doommedraven@gmail.com>
2023-02-08 15:22:16 +0100

    Create whoisxmlapi.py

---
+++ lib/cuckoo/common/integrations/whoisxmlapi.py
@@ -0,0 +1,25 @@
+import logging
+
+import requests
+
+from lib.cuckoo.common.config import Config
+
+log = logging.getLogger()
+externalservices_cfg = Config("externalservices")
+apikey = externalservices_cfg.whoisxmlapi.apikey
+
+def whoisxmlapi_lookup(host):
+    if not externalservices_cfg.whoisxmlapi.enabled or not apikey:
+        return {}
+
+    result = {}
+    log.debug("Performing WHOIS Query for IP/Domain: %s", host)
+    url = f"https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey={apikey}&domainName={host}&outputFormat=json"
+    try:
+        r = requests.get(url, verify=False)
+        if r.ok:
+            result = r.json()
+    except Exception as e:
+        log.error("whoismlapi.com exception: %s", str(e))
+
+    return result
