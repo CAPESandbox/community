6e7c0ae2152c19cf762ba17a934eb6b2ecae29ff
Yasin Tas <yasin.tas@eye.security>
2023-03-30 11:21:18 +0200

    added phishingkit rule for one type of phishing kit

--- modules/signatures/credential_access_phishingkit.py
+++ modules/signatures/credential_access_phishingkit.py
@@ -15,12 +15,18 @@
 
 from lib.cuckoo.common.abstracts import Signature
 
+try:
+    import re2 as re
+except ImportError:
+    import re
+    
+import base64
+
 class PhishingKit(Signature):
-    name = "Phishing_kit_detected"
+    name = "phishing_kit_detected"
     description = "Phishing Kit Detected, sample is trying to harvest credentials"
     severity = 3
     confidence = 100
-    weight = 3
     categories = ["credential_access","evasion","infostealer","phishing", "static"]
     families = ["PhishingKit"]
     authors = ["Yasin Tas",  "Eye Security"]
@@ -31,10 +37,40 @@ class PhishingKit(Signature):
     ttps += ["T1606"]  # MITRE v7,8
     mbcs = ["C0029.003"]  # micro-behaviour
 
-    evented = True
+    def run(self):
+
+        if self.results["info"]["package"] == "edge" or self.results["info"]["package"] == "html":
+            data =  self.results['target']['file']['data']
+            regex_base64 = r'value="([^&]+?)"></input>'
+            decodeString = re.findall(regex_base64,str(data))
+            if decodeString:
+                decoded_url = base64.b64decode(decodeString[0])
+                self.data.append({"decoded_url": decoded_url})
+                decoded_user = base64.b64decode(decodeString[1])
+                self.data.append({"decoded_user": decoded_user})
+            return True
+        
+        return False
+
+class JSAtob(Signature):
+    name = "JS_atob_detected"
+    description = "JS atob Detected, file is obfuscated"
+    severity = 2
+    confidence = 70
+    categories = ["evasion","phishing", "static"]
+    families = ["PhishingKit"]
+    authors = ["Yasin Tas",  "Eye Security"]
+    enabled = True
+    minimum = "1.2"
+    ttps = ["T1140"]  # MITRE v6
+    ttps += ["T1566.001"]  # MITRE v6,7,8
+    mbcs = ["C0029.003"]  # micro-behaviour
 
     def run(self):
-        found_sig = False
-        if "strings" in self.results:
-            found_sig = True
-            return found_sig 
+        if self.results["info"]["package"] == "edge" or self.results["info"]["package"] == "html":
+            data =  self.results['target']['file']['data']
+            if  "atob" in data:
+                return True
+            
+        return False
+        
