17d04a320ff3a800c9eb09e845f7f9bbafd360db
Kevin O'Reilly <kevoreilly@gmail.com>
2023-11-24 09:41:46 +0000

    cryptopools behavioral sig: remove imprecise classification (family name)

--- modules/signatures/cryptopools.py
+++ modules/signatures/cryptopools.py
@@ -7,7 +7,6 @@ class MINERS(Signature):
     name = "cryptopool_domains"
     description = "Connects to crypto currency mining pool"
     severity = 1
-    families = ["miner"]
     categories = ["cryptomining"]
     authors = ["doomedraven", "bartblaze"]
     minimum = "1.2"
