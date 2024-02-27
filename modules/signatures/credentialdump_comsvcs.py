40af186764e87ac220ea1436a8b9a58b030a2a56
kevross33 <kevross33@gmail.com>
2024-02-27 17:32:12 +0000

    Create credentialdump_comsvcs.py
    Credential dumping with Comsvcs sig i.e. "rundll32.exe C:\windows\System32\comsvcs.dll, MiniDump 624 C:\temp\lsass.dmp full"

---
+++ modules/signatures/credentialdump_comsvcs.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2024 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class ComsvcsCredentialDump(Signature):
+    name = "comsvcs_credentialdump"
+    description = "Dumps process memory using comsvcs (likely credential dump of LSASS process)"
+    severity = 3
+    categories = ["credentials"]
+    authors = ["Kevin Ross"]
+    minimum = "1.3"
+    evented = True
+    ttps = ["T1003"]  # # MITRE v6,7,8
+    reference = ["www.ired.team/offensive-security/credential-access-and-credential-dumping/dump-credentials-from-lsass-process-without-mimikatz"]
+
+    def run(self):
+        ret = False
+        cmdlines = self.results["behavior"]["summary"]["executed_commands"]
+        for cmdline in cmdlines:
+            lower = cmdline.lower()
+            if "comsvcs" in lower and "minidump" in lower:
+                ret = True
+                self.data.append({"command": cmdline})
+
+        return ret
