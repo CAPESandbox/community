8ddf995b0016f887d08318784563175bfe2cd8bc
cccs-mog <117194682+cccs-mog@users.noreply.github.com>
2024-05-14 10:07:13 -0400

    Update procmem_yara.py

--- modules/signatures/all/procmem_yara.py
+++ modules/signatures/all/procmem_yara.py
@@ -46,22 +46,25 @@ class Procmem_Yara(Signature):
                     for sub_keyword in ("yara", "cape_yara"):
                         for rule in process.get(sub_keyword, []):
                             if (pid, rule["name"]) not in hits:
-                                hits.append((pid, rule["name"], rule["strings"]))
+                                hits.append((pid, rule["name"], rule["strings"], rule["meta"].get("private")))
 
         for process in self.results.get("CAPE", {}).get("payloads", []) or []:
             pid = process.get("pid", 0)
             for sub_keyword in ("yara", "cape_yara"):
                 for rule in process.get(sub_keyword, []):
                     if (pid, rule["name"]) not in hits:
-                        hits.append((pid, rule["name"], rule["strings"]))
+                        hits.append((pid, rule["name"], rule["strings"], rule["meta"].get("private")))
 
         if hits:
-            for pid, rule, data in hits:
+            for pid, rule, data, private in hits:
                 if rule.lower() in suspicious and self.severity == 3:
                     self.severity = 4
                 elif rule.lower() in malicious and self.severity <= 4:
                     self.severity = 5
-                self.data.append({"Hit": "PID %s trigged the Yara rule '%s' with data '%s'" % (pid, rule, data)})
+                if private:
+                    self.data.append({"Hit": "PID %s trigged the Yara rule '%s'" % (pid, rule)})
+                else:
+                    self.data.append({"Hit": "PID %s trigged the Yara rule '%s' with data '%s'" % (pid, rule, data)})
             return True
 
         return False
