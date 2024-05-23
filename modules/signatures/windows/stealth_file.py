6bc41a5716a5b9a8e1192f64db0696ae400d8019
Michael Bradford <michael.bradford@polyswarm.io>
2024-05-23 13:38:56 -0700

    fix regex

--- modules/signatures/windows/stealth_file.py
+++ modules/signatures/windows/stealth_file.py
@@ -138,9 +138,10 @@ class StealthFile(Signature):
             r"^[A-Z]?:\\Users\\[^\\]+\\AppData\\Local\\Microsoft\\Windows\\History\\History\.IE5\\MSHist[0-9]+\\index\.dat$",
             r"^[A-Z]?:\\Users\\[^\\]+\\AppData\\Local\\Temp\\[^\\]+\.(xls|xlsx|doc|docx|ppt|pttx|tmp)",
             r"^[A-Z]?:\\Users\\[^\\]+\\AppData\\Local\\Adobe\\.*",
-            r"^[A-Z]?:\\Users\\[^\\]+\\AppData\\Roaming\\Microsoft\\Windows\\Recent\\$",
-            r"^[A-Z]?:\\Windows\\SoftwareDistribution\\Download\\[^\\]+\.tmp",
-            r"^[A-Z]?:\\ProgramData\\Microsoft\\ClickToRun\\ProductReleases\\[^\\]+\.tmp",
+            r"^[A-Z]?:\\Users\\[^\\]+\\AppData\\Roaming\\Microsoft\\Windows\\Recent\\"
+            r"^[A-Z]?:\\Windows\\SoftwareDistribution\\Download\\.*\.tmp$",
+            r"^[A-Z]?:\\Windows\\Temp\\.*\.tmp$",
+            r"^[A-Z]?:\\ProgramData\\Microsoft\\ClickToRun\\ProductReleases\\.*\.tmp$",
             r"^[A-Z]?:\\Program Files\\Microsoft Office\\root\\Templates\\[^\\]+\.(xls|xlsx|doc|docx|ppt|pttx|tmp)",
         ]
         url_whitelist = [
