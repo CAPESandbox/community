f01199a630968fe0b4da26561e749b23c43d6a0b
doomedraven <doommedraven@gmail.com>
2025-02-11 09:46:46 +0100

    Update intel_threatfox.py

--- modules/signatures/all/intel_threatfox.py
+++ modules/signatures/all/intel_threatfox.py
@@ -13,10 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import requests
-import urllib3
-import json
-
 from lib.cuckoo.common.abstracts import Signature
 
 class ThreatFox(Signature):
