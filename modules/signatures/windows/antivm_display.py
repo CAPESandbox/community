1842d1f937e1865f951706db045aa2dd63e84f98
kevross33 <kevross33@gmail.com>
2025-12-29 11:21:52 +0000

    Add antivm_display signature
    Add signature to detect use of EnumDisplayDevices such as in Pikabot (https://capesandbox.com/analysis/41245/#)

---
+++ modules/signatures/windows/antivm_display.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2025 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+
+class QueriesDisplayDevices(Signature):
+    name = "queries_display_devices"
+    description = "Queries display devices"
+    severity = 2
+    categories = ["anti-vm"]
+    authors = ["Kevin Ross"]
+    minimum = "1.3"
+    evented = True
+    ttps = ["T1082"]  # MITRE v6,7,8
+
+    filter_apinames = set(["EnumDisplayDevicesA", "EnumDisplayDevicesW"])
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.ret = False
+
+    def on_call(self, call, process):
+        self.mark_call()
+        self.ret = True
+
+    def on_complete(self):
+        return self.ret
