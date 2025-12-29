f6e11eac6c4a206ff8ba14d2b6bf00d5965b626e
kevross33 <kevross33@gmail.com>
2025-12-29 13:01:25 +0000

    NEEDS TESTING: Syscall Signature (fresh ntdll load)
    So this needs testing please as I don't have a current CAPE until i am back in office/have new home lab PC in a few weeks. 
    
    The logic of this is that the Windows loader should take care of NTDLL. Loading a fresh NTDLL into itself from disk likely is then to begin parsing it for syscall instruction for doing that or to bypass hooking.
    
    Here are two samples that do this for testing each element: 
    
    1) AgentTesla https://capesandbox.com/analysis/46120/ 
    
    This uses NTCreateFile on ntdll to load it and then NtReadFile to read it into memory.
    
    2) LUMMA https://capesandbox.com/analysis/46359/
    
    This uses NTCreateFile on ntdll to load it then maps in into its memory with NTCreateSection before changing the memory protections on it (likely to then patch it and remove hooks).

---
+++ modules/signatures/windows/antiedr_syscall.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2025 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+
+class Suspicious_NTDLL_DiskLoad(Signature):
+    name = "suspicious_ntdll_disk_load"
+    description = "Loads clean ntdll.dll from disk (possibly for syscall/anti-EDR)"
+    severity = 3
+    categories = [""syscall", "anti-edr", "unhooking"]
+    authors = ["Kevin Ross"]
+    minimum = "1.3"
+    evented = True
+    ttps = ["T1106"]  # MITRE v6,7,8
+
+    filter_apinames = set(["NtCreateFile", "NtCreateSection", "NtOpenFile", "NtProtectVirtualMemory", "NtReadFile"])
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.ret = False
+        self.filehandle = ""
+        self.mapped = False
+
+    def on_call(self, call, process):
+        if call["api"] in ["NtCreateFile", "NtOpenFile"]:
+            filename = self.get_argument(call, "FileName")
+            if filename.lower() == "c:\windows\system32\ntdll.dll":
+                filehandle = self.get_argument(call, "FileHandle")
+                self.filehandle == filehandle
+                self.mark_call()
+                self.ret = True
+
+        if call["api"] == ("NtReadFile"):
+            filehandle = self.get_argument(call, "FileHandle")
+            handlename = self.get_argument(call, "HandleName")
+            if filehandle == self.filehandle and "ntdll.dll" in handlename.lower():
+                self.mark_call()
+
+        if call["api"] == ("NtCreateSection"):
+             filehandle = self.get_argument(call, "FileHandle")
+             filename = self.get_argument(call, "FileName")
+             if filehandle == self.filehandle and "ntdll.dll" in filename.lower:
+                 self.mapped = True
+                 self.mark_call()
+
+        if call["api"] == ("NtProtectVirtualMemory"):
+            if self.mapped:
+                modulename = self.get_argument(call, "ModuleName")
+                if modulename.lower == "ntdll.dll":
+                    protection = self.get_argument(call, "NewAccessProtection")
+                    # 0x40 PAGE_EXECUTE_READWRITE, 0x80 PAGE_EXECUTE_WRITECOPY, 0x04 PAGE_READWRITE
+                    if protection in ["0x00000040", "0x00000080", "0x00000004"]:
+                        self.mark_call()
+
+    def on_complete(self):
+        return self.ret
